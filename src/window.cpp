e399335656deeed05137bcdf0495ac7c98d8e5b2
Patrick Flynn <patrickf4664@protonmail.com>
2021-04-24 15:33:16 -0400

    Added C and C++ syntax files

--- src/window.cpp
+++ src/window.cpp
@@ -16,7 +16,7 @@ Window::Window() {
     
     QTextEdit *edit = new QTextEdit;
     highlight = new SyntaxHighlighter(edit->document());
-    highlight->setLanguage("cpp");
+    highlight->setLanguage("c");
     this->setCentralWidget(edit);
 }
 
